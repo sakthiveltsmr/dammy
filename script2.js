cf21fce15709b3eb47715b8fae099f3f85db161a
Sakthivel R <43096472+sakthiveltsmr@users.noreply.github.com>
2022-10-06 16:04:24 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,12 @@
+let main=document.getElementsByTagName("body")
+
+
+
+ window.addEventListener("mouseout",(event)=>{
+    if(event.screenY<150){
+        console.log("haii hello");
+        alert("Are you sure you want to Exit")
+    }   
+}
+ )
+    
